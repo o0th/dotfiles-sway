0c60fab643041308c4b2573a52f956f2ae8e5202
o0th <o0th@pm.me>
2022-02-11 09:18:14 +0100

    feat: fish_ssh_agent

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,10 @@
 if status is-interactive
-	fish_ssh_agent
+	if test -z (pgrep ssh-agent)
+		eval (ssh-agent -c) > /dev/null
+		set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+		set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+		set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+	end
 	set -U fish_greeting
 	starship init fish | source
 end
