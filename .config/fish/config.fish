a23c6026784bd4b2ecbbcc64d8a2fabe168836a2
o0th <o0th@pm.me>
2022-02-11 09:21:10 +0100

    feat: fish_ssh_agent

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,7 @@
-if status is-interactive
-	if test -z (pgrep ssh-agent)
-		eval (ssh-agent -c) > /dev/null
-		set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-		set -Ux SSH_AGENT_PID $SSH_AGENT_PID
-		set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-	end
-	set -U fish_greeting
-	starship init fish | source
-end
+# Remove welcome message
+set -U fish_greeting
+
+# Start starship
+starship init fish | source
+
+fish_ssh_agent
